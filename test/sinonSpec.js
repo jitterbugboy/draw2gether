e6b7cfb600bf11e65e058706227b016a65db4bb7
jitterbugboy <jitternielsen@gmail.com>
2013-09-25 20:31:45 +0200

    fix error in unittest sinon

--- test/sinonSpec.js
+++ test/sinonSpec.js
@@ -3,7 +3,7 @@ define(['sinon','utils/sinonTest'], function(sinon, sinonTest) {
     describe('just checking sinon', function() {
 
         it('works for sinon', function() {
-            var mock = sinon.mock(myAPI);
+            //var mock = sinon.mock(myAPI);
               expect(sinonTest).toBeDefined();
 
 
